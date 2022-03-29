d911038253ad727c926635ca5927fd7d2b8f010b
Christian Östman <christianostman@Christians-MacBook-Pro.local>
2022-03-29 11:23:36 +0200

    first

---
+++ Runtime/SoundManager.cs
@@ -0,0 +1,19 @@
+namespace ASmallGame.Sound
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SoundManager : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
